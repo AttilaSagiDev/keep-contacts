bbc0e8f7648aa7afbd1377fb29b76503b04f4549
Attila Sági <attila.sagi@itegration.com>
2024-04-09 11:07:25 +0200

    Add mass delete

---
+++ Controller/Adminhtml/Contacts/MassDelete.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Copyright (c) 2024 Attila Sagi
+ * @license http://www.opensource.org/licenses/mit-license.html  MIT License
+ */
+
+declare(strict_types=1);
+
+namespace Space\KeepContacts\Controller\Adminhtml\Contacts;
+
+use Space\KeepContacts\Controller\Adminhtml\Contacts;
+use Magento\Framework\App\Action\HttpPostActionInterface;
+use Magento\Backend\App\Action\Context;
+use Magento\Ui\Component\MassAction\Filter;
+use Space\KeepContacts\Model\ResourceModel\Contact\CollectionFactory;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Backend\Model\View\Result\Redirect;
+
+class MassDelete extends Contacts implements HttpPostActionInterface
+{
+    /**
+     * @var Filter
+     */
+    private Filter $filter;
+
+    /**
+     * @var CollectionFactory
+     */
+    private CollectionFactory $collectionFactory;
+
+    /**
+     * Constructor
+     *
+     * @param Context $context
+     * @param Filter $filter
+     * @param CollectionFactory $collectionFactory
+     */
+    public function __construct(
+        Context $context,
+        Filter $filter,
+        CollectionFactory $collectionFactory
+    ) {
+        $this->filter = $filter;
+        $this->collectionFactory = $collectionFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * Mass delete action
+     *
+     * @return Redirect|ResponseInterface|ResultInterface
+     */
+    public function execute(): Redirect|ResultInterface|ResponseInterface
+    {
+        try {
+            $collection = $this->filter->getCollection($this->collectionFactory->create());
+            $collectionSize = $collection->getSize();
+
+            foreach ($collection as $contact) {
+                $contact->delete();
+            }
+
+            $this->messageManager->addSuccessMessage(__('A total of %1 record(s) have been deleted.', $collectionSize));
+        } catch (LocalizedException $e) {
+            $this->messageManager->addErrorMessage($e->getMessage());
+        } catch (\Exception $e) {
+            $this->messageManager->addExceptionMessage(
+                $e,
+                __('Something went wrong while deleting the selected items.')
+            );
+        }
+
+        /** @var Redirect $resultRedirect */
+        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+        return $resultRedirect->setPath('*/*/');
+    }
+}
