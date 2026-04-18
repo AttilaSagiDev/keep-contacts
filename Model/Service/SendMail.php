5cd20c4346e7837dc852556be246c7041ba6214b
Attila Sagi <attila.sagi.dev@gmail.com>
2026-04-18 13:14:18 +0200

    Add refactor and unit tests

--- Model/Service/SendMail.php
+++ Model/Service/SendMail.php
@@ -69,7 +69,7 @@ class SendMail
             )->setTemplateOptions(
                 [
                     'area' => Area::AREA_FRONTEND,
-                    'store' => $this->storeManager->getStore((int)$contact->getStoreId())->getStoreId()
+                    'store' => $this->storeManager->getStore((int)$contact->getStoreId())->getId()
                 ]
             )->setTemplateVars(
                 [
